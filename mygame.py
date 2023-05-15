3b979bc33855382ef205d2fa5b3bd1be1f7ce9a2
Ryan Barnes <ryan.bayangan@gmail.com>
2023-05-16 09:31:43 +1000

    feat: refactor and improve logic

--- mygame.py
+++ mygame.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
+import random
+import time
 
 import pygame
+
 import objects
-from objects import Vector
-import time
 from colour import Colour
+from objects import Vector
 
 # Global Declare
 g_displayWidth = 500
 g_displayHeight = 600
-g_display = Vector(g_displayWidth,g_displayHeight)
+g_display = Vector(g_displayWidth, g_displayHeight)
 
 # Create Pygame Window
 g_displayWindow = pygame.display.set_mode(g_display.t())
@@ -17,170 +19,197 @@ g_gameClock = pygame.time.Clock()
 pygame.init()
 pygame.display.set_caption("PyRacing")
 
+
 def gameLoop(gameDisplay):
-	gameExit = False
-	gamePaused = True
-	
-	hlanes = [75,170,255,325,418]
-	vlanes = [250,0,500,300,0]
-	lanespeed = [1,0.5,1.5,0.8,1.8]
-	score = 0
-	x_change = 0
-	y_change = 0
-	keyRIGHT = 0
-	keyLEFT = 0
-	keyUP = 0
-	keyDOWN = 0
-	passed = 0
-	dif = 2
-	difmod = 0
-	score = 0
-	highscore = 0
-	
-	# Load background
-	for x in range(2):
-		BG = objects.Background('assets/fullroad.png',Vector(500,600),Vector(500,600))
-		BG.offset = 600 * x
-		
-	# Load Player Car	
-	P1 = objects.Car('assets/racecar1.png',Vector(120,120),Vector(64,100))
-	P1.name = 'player'
+    gameExit = False
+    gamePaused = True
 
-	# Set Stating position
-	startPos = (g_display / 2) - (P1.box / 2)
-	P1.position = startPos
+    hlanes = [75, 170, 255, 325, 418]
+    vlanes = [-120, -120, -120, -120, -120]
+    lanespeed = [0, 0, 0, 0, 0]
+    score = 0
+    x_change = 0
+    y_change = 0
+    keyRIGHT = 0
+    keyLEFT = 0
+    keyUP = 0
+    keyDOWN = 0
+    passed = 0
+    dif = 2
+    difmod = 0
+    score = 0
+    highscore = 0
+    opponents = list(range(5))
 
-	# Load Obstacles
-	for x in range(5):
-		newOp = objects.Opponent('assets/racecar2.png',Vector(120,120),Vector(64,100))
-		newOp.speed = lanespeed[x]
-		newOp.hoffset = hlanes[x] - (newOp.boxwidth() / 2)
-		newOp.voffset = vlanes[x]
-		newOp.setpos(Vector(newOp.hoffset,newOp.voffset))
-		newOp.name = 'opp%i' % x
+    # Load background
+    for x in range(2):
+        BG = objects.Background(
+            "assets/fullroad.png", Vector(500, 600), Vector(500, 600)
+        )
+        BG.offset = 600 * x
 
+    # Load Player Car
+    P1 = objects.Car("assets/racecar1.png", Vector(120, 120), Vector(64, 100))
+    P1.name = "player"
 
-	# GameLoop
-	while not gameExit:
-		# Read Events
-		for event in pygame.event.get():
-			# Exit if window quits
-			if event.type == pygame.QUIT:
-				gameExit = True
+    # Set Stating position
+    startPos = (g_display / 2) - (P1.box / 2)
+    P1.position = startPos
 
-			# Read key Values
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_LEFT:
-					keyLEFT = 1
-				elif event.key == pygame.K_RIGHT:
-					keyRIGHT = 1
-				elif event.key == pygame.K_UP:
-					keyUP = 1
-				elif event.key == pygame.K_DOWN:
-					keyDOWN = 1
-				elif event.key == pygame.K_SPACE:
-					gamePaused = not gamePaused
-			if event.type == pygame.KEYUP:
-				if event.key == pygame.K_LEFT:
-					keyLEFT = 0
-				elif event.key == pygame.K_RIGHT:
-					keyRIGHT = 0
-				elif event.key == pygame.K_UP:
-					keyUP = 0
-				elif event.key == pygame.K_DOWN:
-					keyDOWN = 0
-				elif event.key == pygame.K_ESCAPE:
-					gameExit = True
+    # Load Obstacles
+    for x in range(5):
+        newOp = objects.Opponent(
+            "assets/racecar2.png",
+            Vector(120, 120),
+            Vector(64, 100),
+        )
+        newOp.speed = lanespeed[x]
+        newOp.hoffset = hlanes[x] - (newOp.boxwidth() / 2)
+        newOp.voffset = vlanes[x]
+        newOp.setpos(Vector(newOp.hoffset, newOp.voffset))
+        newOp.name = "opp%i" % x
 
-		# Run Simulation
-		if not gamePaused:
-			# ADJUST MOVE DELTA
-			if x_change != 0 and keyRIGHT - keyLEFT == 0:
-				# DECREASE X SPEED
-				x_change = round(x_change - ((x_change / abs(x_change)) / 4),3)
-			else:
-				# INCREASE X SPEED
-				x_change = max(min(x_change + ((keyRIGHT - keyLEFT) / 2),5),-5)
+    # GameLoop
+    while not gameExit:
+        # Read Events
+        for event in pygame.event.get():
+            # Exit if window quits
+            if event.type == pygame.QUIT:
+                gameExit = True
 
-			if y_change != 0 and keyDOWN - keyUP == 0:
-				# DECREASE Y SPEED
-				y_change = round(y_change - ((y_change / abs(y_change)) / 10),3)
-			else:
-				# INCREASE Y SPEED
-				y_change = max(min(y_change + ((keyDOWN - keyUP) / 2),4),-4)
+            # Read key Values
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    keyLEFT = 1
+                elif event.key == pygame.K_RIGHT:
+                    keyRIGHT = 1
+                elif event.key == pygame.K_UP:
+                    keyUP = 1
+                elif event.key == pygame.K_DOWN:
+                    keyDOWN = 1
+                elif event.key == pygame.K_SPACE:
+                    gamePaused = not gamePaused
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    keyLEFT = 0
+                elif event.key == pygame.K_RIGHT:
+                    keyRIGHT = 0
+                elif event.key == pygame.K_UP:
+                    keyUP = 0
+                elif event.key == pygame.K_DOWN:
+                    keyDOWN = 0
+                elif event.key == pygame.K_ESCAPE:
+                    gameExit = True
 
-			# CHECK POSITION
-			car_pos = P1.position
-			 
-			car_Nx = car_pos.x + x_change
-			car_Ny = car_pos.y + y_change
+        # Start running cars
+        while len(opponents) > 2:
+            chosen = random.choice(opponents)
+            opponents.remove(chosen)
+            objects.Opponent.collection.members[chosen].speed = (
+                random.random() * 2
+            ) + 0.5
 
-			for o in objects.Opponent.collection.members:
-				o.move(Vector(0,(dif + difmod) * o.speed))
-				if o.position.y > g_displayHeight:
-					o.setpos(Vector(o.position.x,-o.boxheight()))
-					score += 1
-				collided = P1.hascollided(o)
-				if collided[0]:
-					if collided[1] != 0:
-						if o.center().y > P1.center().y:
-							x = -1
-						else:
-							x = 1
-						y_change = ((dif + difmod) * o.speed) * x
-					if collided[1] > collided[2]:
-						if o.center().x > P1.center().x:
-							x = -1
-						else:
-							x = 1
-						x_change = x * (abs(x_change) + 0.5)
+        # Run Simulation
+        if not gamePaused:
+            # ADJUST MOVE DELTA
+            if x_change != 0 and keyRIGHT - keyLEFT == 0:
+                # DECREASE X SPEED
+                x_change = round(x_change - ((x_change / abs(x_change)) / 4), 3)
+            else:
+                # INCREASE X SPEED
+                x_change = max(min(x_change + ((keyRIGHT - keyLEFT) / 2), 5), -5)
 
-			# CHECK AND CHANGE X POSITION
-			if car_Nx > g_displayWidth - P1.boxwidth() + (P1.width() / 2) or car_Nx < -(P1.width() / 2):
-				x_change = 0
-			
-			# CHECK FOR Y CONDITIONS
-			if car_Ny > g_displayHeight - P1.height():
-				# PLAYER HIT BOTTOM OF SCREEN : DISPLAY SCORE AND RESET
-				highscore = max(score,highscore)
-				msgtxt = 'Score: %i/%i' % (score,highscore)
-				msg = objects.TextObject(msgtxt,'freesansbold.ttf',40,Colour.Red,g_display / 2)
-				msg.drawcenter = True
-				msg.draw(gameDisplay)
-				pygame.display.update()
-				P1.setang(0)
-				P1.setpos(startPos)
-				x_change = 0
-				y_change = 0
-				difmod = 0
-				passed = 0
-				time.sleep(2)
-				del msg
-				score = 0
-				for Op in objects.Opponent.collection.members:
-					Op.setpos(Vector(Op.hoffset,Op.voffset))
-				gamePaused = True
-			elif car_Ny <= 0:
-				# PLAYER HIT TOP OF SCREEN
-				y_change = 1
+            if y_change != 0 and keyDOWN - keyUP == 0:
+                # DECREASE Y SPEED
+                y_change = round(y_change - ((y_change / abs(y_change)) / 10), 3)
+            else:
+                # INCREASE Y SPEED
+                y_change = max(min(y_change + ((keyDOWN - keyUP) / 2), 4), -4)
 
-			# MOVE PLAYER
-			P1.move(Vector(x_change,y_change + ((dif + difmod) / 2) + (abs(keyRIGHT - keyLEFT) / 2)))
-				
-			# MOVE OPPONENTS
-			for b in objects.Background.collection.members:
-				b.setpos(Vector(0,((passed * 2.5) % g_displayHeight) - b.offset))
-			
-			# INCREMENT PASSED TIME AND INCREASE DIFFICULTY	
-			passed += dif + difmod
-			difmod = round(min(3.9,difmod + 0.002),3)
+            # CHECK POSITION
+            car_pos = P1.position
 
-		#DRAW TO FRAME
-		objects.Sprite.All.draw(gameDisplay)
+            car_Nx = car_pos.x + x_change
+            car_Ny = car_pos.y + y_change
 
-		#UPDATE DISPLAY
-		pygame.display.update()
-		g_gameClock.tick(60)
+            for i, o in enumerate(objects.Opponent.collection.members):
+                o.move(Vector(0, (dif + difmod) * o.speed))
+                if o.position.y > g_displayHeight:
+                    o.speed = 0
+                    o.setpos(Vector(o.position.x, -o.boxheight()))
+                    opponents.append(i)
+                    score += 1
+                collided = P1.hascollided(o)
+                if collided[0]:
+                    if collided[1] != 0:
+                        if o.center().y > P1.center().y:
+                            x = -1
+                        else:
+                            x = 1
+                        y_change = ((dif + difmod) * o.speed) * x
+                    if collided[1] > collided[2]:
+                        if o.center().x > P1.center().x:
+                            x = -1
+                        else:
+                            x = 1
+                        x_change = x * (abs(x_change) + 0.5)
+
+            # CHECK AND CHANGE X POSITION
+            if car_Nx > g_displayWidth - P1.boxwidth() + (P1.width() / 2) or car_Nx < -(
+                P1.width() / 2
+            ):
+                x_change = 0
+
+            # CHECK FOR Y CONDITIONS
+            if car_Ny > g_displayHeight - P1.height():
+                # PLAYER HIT BOTTOM OF SCREEN : DISPLAY SCORE AND RESET
+                highscore = max(score, highscore)
+                msgtxt = "Score: %i/%i" % (score, highscore)
+                msg = objects.TextObject(
+                    msgtxt, "freesansbold.ttf", 40, Colour.Red, g_display / 2
+                )
+                msg.drawcenter = True
+                msg.draw(gameDisplay)
+                pygame.display.update()
+                P1.setang(0)
+                P1.setpos(startPos)
+                x_change = 0
+                y_change = 0
+                difmod = 0
+                passed = 0
+                time.sleep(2)
+                del msg
+                score = 0
+                for Op in objects.Opponent.collection.members:
+                    Op.setpos(Vector(Op.hoffset, Op.voffset))
+                gamePaused = True
+            elif car_Ny <= 0:
+                # PLAYER HIT TOP OF SCREEN
+                y_change = 1
+
+            # MOVE PLAYER
+            P1.move(
+                Vector(
+                    x_change,
+                    y_change + ((dif + difmod) / 2) + (abs(keyRIGHT - keyLEFT) / 2),
+                )
+            )
+
+            # MOVE OPPONENTS
+            for b in objects.Background.collection.members:
+                b.setpos(Vector(0, ((passed * 2.5) % g_displayHeight) - b.offset))
+
+            # INCREMENT PASSED TIME AND INCREASE DIFFICULTY
+            passed += dif + difmod
+            difmod = round(min(3.9, difmod + 0.002), 3)
+
+        # DRAW TO FRAME
+        objects.Sprite.All.draw(gameDisplay)
+
+        # UPDATE DISPLAY
+        pygame.display.update()
+        g_gameClock.tick(60)
+
 
 # START GAME LOOP
 gameLoop(g_displayWindow)
--- objects.py
+++ objects.py
@@ -1,186 +1,237 @@
 #!/usr/bin/python3
 
-import pygame
 import math
 
-class Vector():
-	def __init__(self,x=0,y=0):
-		self.x = x
-		self.y = y
-		return None
-	def __repr__(self):
-		return (self.x,self.y)
-	def __str__(self):
-		return str(self.__repr__())
-	def __add__(self,offset):
-		if isinstance(offset,Vector):
-			return Vector(self.x + offset.x,self.y + offset.y)
-		else:
-			return Vector(self.x + offset,self.y + offset)
-	def __sub__(self,offset):
-		if isinstance(offset,Vector):
-			return Vector(self.x - offset.x,self.y - offset.y)
-		else:
-			return Vector(self.x - offset,self.y - offset)
-	def __mul__(self,offset):
-		if isinstance(offset,Vector):
-			return Vector(self.x * offset.x,self.y * offset.y)
-		else:
-			return Vector(self.x * offset,self.y * offset)
-	def __truediv__(self,offset):
-		if isinstance(offset,Vector):
-			return Vector(self.x / offset.x,self.y / offset.y)
-		else:
-			return Vector(self.x / offset,self.y / offset)
-	def distance(self,other):
-		a = 0
-		for b, c in zip(self.__repr__(),other.__repr__()) : a += ((c - b) ** 2)
-		return math.sqrt(a)
-	def direction(self,other):
-		d = [a - b for a,b in zip(other.__repr__(),self.__repr__())]
-		rad = math.atan2(d[0],d[1])
-		return rad * (180 / math.pi)
-	def t(self):
-		return (self.x,self.y)
+import pygame
 
-class Group():
-	def __init__(self):
-		self.members = []
-	def add(self,obj,i=None):
-		if obj not in self.members:
-			if i == None:
-				self.members.append(obj)
-			else:
-				self.members.insert(i,obj)
-	def remove(self,obj):
-		if obj in self.members:
-			self.members.remove(obj)
-	def shift(self,obj,i):
-		if obj in self.members:
-			hold = obj
-			self.members.remove(obj)
-			self.members.insert(i,obj)
-	def move(self,offset):
-		for obj in self.members:
-			obj.move(offset)
-	def draw(self,d):
-		for obj in self.members:
-			obj.draw(d)
 
-class Sprite():
-	All = Group()
+class Vector:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+        return None
 
-	def __init__(self,pth,box,size):
-		self.img = pygame.image.load(pth)
-		self.size = size
-		self.box = box
-		self.angle = 0
-		self.position = Vector()
-		self.name = ':'
-		Sprite.All.add(self)
-	def __del__(self):
-		Sprite.All.remove(self)
-	def __getitem__(self,index):
-		return None
-	def draw(self,d):
-		d.blit(self.img,self.position.t())
-		return None
-	def move(self,offset):
-		self.position = self.position + offset
-		return None
-	def setpos(self,target):
-		self.position = target
-		return None
-	def setang(self,target):
-		self.angle = target
-		return None
-	def width(self):
-		return self.size.x
-	def height(self):
-		return self.size.y
-	def boxwidth(self):
-		return self.box.x
-	def boxheight(self):
-		return self.box.y
-	def center(self):
-		return self.position - (self.box / 2)
+    def __repr__(self):
+        return (self.x, self.y)
 
-class TextObject():
-	def __init__(self,text,font,fontsize,colour,pos):
-		self.text = text
-		self.position = pos
-		self.font = font
-		self.fontsize = fontsize
-		self.colour = colour
-		self.size = Vector()
-		self.drawcenter = False
-		return None
-	def text_objects(self, text, font):
-		textSurface = font.render(text, True, self.colour)
-		return textSurface, textSurface.get_rect()
-	def draw(self,d):
-		myfont = pygame.font.Font(self.font,self.fontsize)
-		TextSurf, TextRect = self.text_objects(self.text, myfont)
-		if self.drawcenter:
-			TextRect.center = self.position.t()
-		else:
-			TextRect.position = self.position.t()
-		d.blit(TextSurf, TextRect)
-		return None
-	def __enter__(self):
-		return None
-	def __exit__(self):
-		return None
+    def __str__(self):
+        return str(self.__repr__())
+
+    def __add__(self, offset):
+        if isinstance(offset, Vector):
+            return Vector(self.x + offset.x, self.y + offset.y)
+        else:
+            return Vector(self.x + offset, self.y + offset)
+
+    def __sub__(self, offset):
+        if isinstance(offset, Vector):
+            return Vector(self.x - offset.x, self.y - offset.y)
+        else:
+            return Vector(self.x - offset, self.y - offset)
+
+    def __mul__(self, offset):
+        if isinstance(offset, Vector):
+            return Vector(self.x * offset.x, self.y * offset.y)
+        else:
+            return Vector(self.x * offset, self.y * offset)
+
+    def __truediv__(self, offset):
+        if isinstance(offset, Vector):
+            return Vector(self.x / offset.x, self.y / offset.y)
+        else:
+            return Vector(self.x / offset, self.y / offset)
+
+    def distance(self, other):
+        a = 0
+        for b, c in zip(self.__repr__(), other.__repr__()):
+            a += (c - b) ** 2
+        return math.sqrt(a)
+
+    def direction(self, other):
+        d = [a - b for a, b in zip(other.__repr__(), self.__repr__())]
+        rad = math.atan2(d[0], d[1])
+        return rad * (180 / math.pi)
+
+    def t(self):
+        return (self.x, self.y)
+
+
+class Group:
+    def __init__(self):
+        self.members = []
+
+    def add(self, obj, i=None):
+        if obj not in self.members:
+            if i == None:
+                self.members.append(obj)
+            else:
+                self.members.insert(i, obj)
+
+    def remove(self, obj):
+        if obj in self.members:
+            self.members.remove(obj)
+
+    def shift(self, obj, i):
+        if obj in self.members:
+            hold = obj
+            self.members.remove(obj)
+            self.members.insert(i, obj)
+
+    def move(self, offset):
+        for obj in self.members:
+            obj.move(offset)
+
+    def draw(self, d):
+        for obj in self.members:
+            obj.draw(d)
+
+
+class Sprite:
+    All = Group()
+
+    def __init__(self, pth, box, size):
+        self.img = pygame.image.load(pth)
+        self.size = size
+        self.box = box
+        self.angle = 0
+        self.position = Vector()
+        self.name = ":"
+        Sprite.All.add(self)
+
+    def __del__(self):
+        Sprite.All.remove(self)
+
+    def __getitem__(self, index):
+        return None
+
+    def draw(self, d):
+        d.blit(self.img, self.position.t())
+        return None
+
+    def move(self, offset):
+        self.position = self.position + offset
+        return None
+
+    def setpos(self, target):
+        self.position = target
+        return None
+
+    def setang(self, target):
+        self.angle = target
+        return None
+
+    def width(self):
+        return self.size.x
+
+    def height(self):
+        return self.size.y
+
+    def boxwidth(self):
+        return self.box.x
+
+    def boxheight(self):
+        return self.box.y
+
+    def center(self):
+        return self.position - (self.box / 2)
+
+
+class TextObject:
+    def __init__(self, text, font, fontsize, colour, pos):
+        self.text = text
+        self.position = pos
+        self.font = font
+        self.fontsize = fontsize
+        self.colour = colour
+        self.size = Vector()
+        self.drawcenter = False
+        return None
+
+    def text_objects(self, text, font):
+        textSurface = font.render(text, True, self.colour)
+        return textSurface, textSurface.get_rect()
+
+    def draw(self, d):
+        myfont = pygame.font.Font(self.font, self.fontsize)
+        TextSurf, TextRect = self.text_objects(self.text, myfont)
+        if self.drawcenter:
+            TextRect.center = self.position.t()
+        else:
+            TextRect.position = self.position.t()
+        d.blit(TextSurf, TextRect)
+        return None
+
+    def __enter__(self):
+        return None
+
+    def __exit__(self):
+        return None
+
 
 class Car(Sprite):
-	type = 'Car'
+    type = "Car"
 
-	def hascollided(self,obj):
-		b = (self.height() / 2) + (obj.height() / 2) - abs(self.center().y - obj.center().y)
-		c = (self.width() / 2) + (obj.width() / 2) - abs(self.center().x - obj.center().x)
-		return b >= 0 and c >= 0, b, c
-	def rotate(self,image,angle):
-	    orig_rect = image.get_rect()
-	    rot_image = pygame.transform.rotate(image, angle)
-	    rot_rect = orig_rect.copy()
-	    rot_rect.center = rot_image.get_rect().center
-	    rot_image = rot_image.subsurface(rot_rect).copy()
-	    return rot_image
-	def move(self,offset):
-		self.angle = offset.x * -1.5
-		self.position = self.position + offset
-		return None
-	def draw(self,d):
-		o = self.rotate(self.img,self.angle)
-		d.blit(o,self.position.t())
-		return None
+    def hascollided(self, obj):
+        b = (
+            (self.height() / 2)
+            + (obj.height() / 2)
+            - abs(self.center().y - obj.center().y)
+        )
+        c = (
+            (self.width() / 2)
+            + (obj.width() / 2)
+            - abs(self.center().x - obj.center().x)
+        )
+        return b >= 0 and c >= 0, b, c
+
+    def rotate(self, image, angle):
+        orig_rect = image.get_rect()
+        rot_image = pygame.transform.rotate(image, angle)
+        rot_rect = orig_rect.copy()
+        rot_rect.center = rot_image.get_rect().center
+        rot_image = rot_image.subsurface(rot_rect).copy()
+        return rot_image
+
+    def move(self, offset):
+        self.angle = offset.x * -1.5
+        self.position = self.position + offset
+        return None
+
+    def draw(self, d):
+        o = self.rotate(self.img, self.angle)
+        d.blit(o, self.position.t())
+        return None
+
 
 class Background(Sprite):
-	collection = Group()
+    collection = Group()
 
-	def __init__(self,pth,box,size):
-		self.img = pygame.image.load(pth)
-		self.size = size
-		self.box = box
-		self.angle = 0
-		self.position = Vector()
-		Sprite.All.add(self)
-		Background.collection.add(self)
-	def __del__(self):
-		Sprite.All.remove(self)
-		Background.collection.remove(self)
+    def __init__(self, pth, box, size):
+        self.img = pygame.image.load(pth)
+        self.size = size
+        self.box = box
+        self.angle = 0
+        self.position = Vector()
+        Sprite.All.add(self)
+        Background.collection.add(self)
+
+    def __del__(self):
+        Sprite.All.remove(self)
+        Background.collection.remove(self)
+
 
 class Opponent(Car):
-	collection = Group()
+    collection = Group()
 
-	def __init__(self,pth,box,size):
-		self.img = pygame.image.load(pth)
-		self.size = size
-		self.box = box
-		self.angle = 0
-		self.position = Vector()
-		Sprite.All.add(self)
-		Opponent.collection.add(self)
-	def __del__(self):
-		Sprite.All.remove(self)
-		Opponent.collection.remove(self)
+    def __init__(self, pth, box, size):
+        self.img = pygame.image.load(pth)
+        self.size = size
+        self.box = box
+        self.angle = 0
+        self.position = Vector()
+        Sprite.All.add(self)
+        Opponent.collection.add(self)
+
+    def __del__(self):
+        Sprite.All.remove(self)
+        Opponent.collection.remove(self)
